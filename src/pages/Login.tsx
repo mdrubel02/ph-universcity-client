fbbba2f70e51325f934612651e7086b0c835d578
mdrubel02 <mdrubel072125@gmail.com>
2024-02-23 14:47:00 +0600

    'module-tow-complite'

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,30 @@
-import { Button } from "antd"
-import { FieldValues, useForm } from "react-hook-form"
+import { Button, Row } from "antd";
+// import { FieldValues, useForm } from "react-hook-form"
 import { useGetLoginMutation } from "../redux/features/auth/authApi"
 import { useAppDispatch } from "../redux/hooks"
 import { TUser, setUser } from "../redux/features/auth/authSlice"
 import { verifyToken } from "../utils/verifyToken"
 import { useNavigate } from "react-router-dom"
 import { toast } from "sonner"
+import { FieldValues } from "react-hook-form";
+import PHForm from "../components/Form/PHForm";
+import PHInput from "../components/Form/PHInput";
+ 
 
+type TDefaultValues = {
+  userId: string,
+  password: string
+}
 const Login = () => {
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
-  const { register, handleSubmit } = useForm()
+  // const { register, handleSubmit } = useForm()
 
-  const [login, {  error }] = useGetLoginMutation()
+  const [login] = useGetLoginMutation()
   // console.log('data =>', data)
-  console.log('error =>', error)
+  // console.log('error =>', error)
   const onSubmit = async (data: FieldValues) => {
+    console.log("data =>", data);
     const toastId = toast.loading('Logging in');
     try {
       const userInfo = {
@@ -30,21 +39,22 @@ const Login = () => {
     } catch (err) {
       toast.error('Something went wrong', { id: toastId, duration: 2000 });
     }
-
+  };
+  const defaultValues : TDefaultValues= {
+    userId:"A-0001",
+    password:"admin123" 
   }
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="id">id:</label>
-        <input type="text" {...register('userId')} id="id" />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" {...register('password')} id="password" />
-      </div>
-      <Button htmlType="submit">Login</Button>
-    </form>
-  )
-}
+    <Row justify='center' align='middle' style={{height: '100vh'}}>
+      <PHForm onSubmit={onSubmit} defaultValues={defaultValues}>
+          {/* <label htmlFor="id">id:</label> */}
+          {/* <input type="text" {...register('userId')} id="id" /> */}
+          <PHInput type="text" name="userId" label="ID" />
+          <PHInput type="password" name="password" label="Password" />
+        <Button htmlType="submit">Login</Button>
+      </PHForm>
+    </Row>
+  );
+};
 
-export default Login
+export default Login;
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,11 +1,50 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BaseQueryApi, BaseQueryFn, DefinitionType, FetchArgs, createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {  RootState } from "../store";
+import { logout, setUser } from "../features/auth/authSlice";
+
+const apiBaseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/api/v1",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+    return headers;
+  },
+});
 
 
+const baseQueryWithRefreshToken : BaseQueryFn<FetchArgs,BaseQueryApi,DefinitionType> = async (args, api, extraOption) : Promise<any> => {
+    let result =await apiBaseQuery(args,api,extraOption)
+    console.log(result)
+
+    if(result?.error?.status === 401){
+
+        const res = await fetch('http://localhost:5000/api/v1/auth//refresh-token',{
+            method: 'POST',
+            credentials: 'include'
+        });
+        const data = await res.json();
+        if(data?.data?.accessToken){
+            const user = (api.getState() as RootState).auth.user;
+        api.dispatch(
+            setUser({
+                user,
+                token: data.accessToken
+            })
+        ) 
+        }else{
+            api.dispatch(logout()) 
+        }
+
+        result = await  apiBaseQuery(args,api,extraOption)
+    }
+
+    return result;
+}
 export const baseApi = createApi({
-    reducerPath: 'baseApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000/api/v1',
-    credentials: 'include',
-}),
-    endpoints : ()=> ({})
-})
-
+  reducerPath: "baseApi",
+  baseQuery: baseQueryWithRefreshToken,
+  endpoints: () => ({}),
+});
